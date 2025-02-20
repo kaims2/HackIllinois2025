0edf0d0c33127ff57e4cdb7135cd8e9117ccbe3b
James Kabbes <james.kabbes@gmail.com>
2025-02-20 09:15:38 -0600

    adding custom source code

---
+++ src/scout/constants.py
@@ -0,0 +1,7 @@
+from scout import types
+
+LED_PINS: types.LEDPinsConfig = {
+    'red': 22,
+    'green': 27,
+    'blue': 24
+}
---
+++ src/scout/types.py
@@ -0,0 +1,7 @@
+import typing
+
+
+class LEDPinsConfig(typing.TypedDict):
+    red: int
+    green: int
+    blue: int
