f56369259ea1d9f832c0de26e8efa16a18aaa2cd
James Kabbes <james.kabbes@gmail.com>
2025-02-21 17:30:57 -0600

    will need to physically override the pin layout for the shield for this to work

--- src/scout/line_sensors.py
+++ src/scout/line_sensors.py
@@ -4,31 +4,17 @@ from scout import constants
 
 class LineSensors:
 
-    # far_left_sensor: gpiozero.LineSensor
-    # middle_left_sensor: gpiozero.LineSensor
-    # middle_right_sensor: gpiozero.LineSensor
-    # far_right_sensor: gpiozero.LineSensor
-
-    far_left_sensor: gpiozero.DigitalInputDevice
-    middle_left_sensor: gpiozero.DigitalInputDevice
-    middle_right_sensor: gpiozero.DigitalInputDevice
-    far_right_sensor: gpiozero.DigitalInputDevice
+    far_left_sensor: gpiozero.LineSensor
+    middle_left_sensor: gpiozero.LineSensor
+    middle_right_sensor: gpiozero.LineSensor
+    far_right_sensor: gpiozero.LineSensor
 
     def __init__(self):
-        # self.far_left_sensor = gpiozero.LineSensor(
-        #     constants.LINE_SENSOR_PINS['far_left'])
-        # self.middle_left_sensor = gpiozero.LineSensor(
-        #     constants.LINE_SENSOR_PINS['middle_left'])
-        # self.middle_right_sensor = gpiozero.LineSensor(
-        #     constants.LINE_SENSOR_PINS['middle_right'])
-        # self.far_right_sensor = gpiozero.LineSensor(
-        #     constants.LINE_SENSOR_PINS['far_right'])
-
-        self.far_left_sensor = gpiozero.DigitalInputDevice(
+        self.far_left_sensor = gpiozero.LineSensor(
             constants.LINE_SENSOR_PINS['far_left'])
-        self.middle_left_sensor = gpiozero.DigitalInputDevice(
+        self.middle_left_sensor = gpiozero.LineSensor(
             constants.LINE_SENSOR_PINS['middle_left'])
-        self.middle_right_sensor = gpiozero.DigitalInputDevice(
+        self.middle_right_sensor = gpiozero.LineSensor(
             constants.LINE_SENSOR_PINS['middle_right'])
-        self.far_right_sensor = gpiozero.DigitalInputDevice(
+        self.far_right_sensor = gpiozero.LineSensor(
             constants.LINE_SENSOR_PINS['far_right'])
