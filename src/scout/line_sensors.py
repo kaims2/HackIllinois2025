dab3958812cca987bdbbca20d1d6720bb86075fe
James Kabbes <james.kabbes@gmail.com>
2025-02-21 17:27:54 -0600

    testing DigitalInputDevice instead

--- src/scout/line_sensors.py
+++ src/scout/line_sensors.py
@@ -4,17 +4,31 @@ from scout import constants
 
 class LineSensors:
 
-    far_left_sensor: gpiozero.LineSensor
-    middle_left_sensor: gpiozero.LineSensor
-    middle_right_sensor: gpiozero.LineSensor
-    far_right_sensor: gpiozero.LineSensor
+    # far_left_sensor: gpiozero.LineSensor
+    # middle_left_sensor: gpiozero.LineSensor
+    # middle_right_sensor: gpiozero.LineSensor
+    # far_right_sensor: gpiozero.LineSensor
+
+    far_left_sensor: gpiozero.DigitalInputDevice
+    middle_left_sensor: gpiozero.DigitalInputDevice
+    middle_right_sensor: gpiozero.DigitalInputDevice
+    far_right_sensor: gpiozero.DigitalInputDevice
 
     def __init__(self):
-        self.far_left_sensor = gpiozero.LineSensor(
+        # self.far_left_sensor = gpiozero.LineSensor(
+        #     constants.LINE_SENSOR_PINS['far_left'])
+        # self.middle_left_sensor = gpiozero.LineSensor(
+        #     constants.LINE_SENSOR_PINS['middle_left'])
+        # self.middle_right_sensor = gpiozero.LineSensor(
+        #     constants.LINE_SENSOR_PINS['middle_right'])
+        # self.far_right_sensor = gpiozero.LineSensor(
+        #     constants.LINE_SENSOR_PINS['far_right'])
+
+        self.far_left_sensor = gpiozero.DigitalInputDevice(
             constants.LINE_SENSOR_PINS['far_left'])
-        self.middle_left_sensor = gpiozero.LineSensor(
+        self.middle_left_sensor = gpiozero.DigitalInputDevice(
             constants.LINE_SENSOR_PINS['middle_left'])
-        self.middle_right_sensor = gpiozero.LineSensor(
+        self.middle_right_sensor = gpiozero.DigitalInputDevice(
             constants.LINE_SENSOR_PINS['middle_right'])
-        self.far_right_sensor = gpiozero.LineSensor(
+        self.far_right_sensor = gpiozero.DigitalInputDevice(
             constants.LINE_SENSOR_PINS['far_right'])
