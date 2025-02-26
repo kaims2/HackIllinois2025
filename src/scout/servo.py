757307831f12bfdc12578974d0f11804121a83c5
James Kabbes <james.kabbes@gmail.com>
2025-02-26 16:57:14 -0600

    adding initial angle

--- src/scout/servo.py
+++ src/scout/servo.py
@@ -4,4 +4,4 @@ import gpiozero
 class Servo(gpiozero.AngularServo):
     def __init__(self, pin):
         super().__init__(pin=pin, max_angle=180, min_angle=0,
-                         min_pulse_width=0.0005, max_pulse_width=0.0025)
+                         min_pulse_width=0.0005, max_pulse_width=0.0025, initial_angle=90)
