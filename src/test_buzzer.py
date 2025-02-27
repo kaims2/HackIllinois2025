730c66481e67f2da5f7d4dc3de6d3b664b551327
James Kabbes <james.kabbes@gmail.com>
2025-02-27 14:17:12 -0600

    adding buzzer test

---
+++ src/test_buzzer.py
@@ -0,0 +1,18 @@
+from scout import constants
+import time
+import signal
+import gpiozero
+
+if __name__ == '__main__':
+
+    buzzer = gpiozero.Buzzer(constants.BUZZER_PIN)
+
+    def signal_handler(sig, frame):
+        buzzer.off()
+        exit(0)
+
+    signal.signal(signal.SIGINT, signal_handler)
+
+    buzzer.on()
+    time.sleep(1)
+    buzzer.off()
