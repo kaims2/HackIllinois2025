696a4d741da7c74f2c206c4e4abb0256205887f7
James Kabbes <james.kabbes@gmail.com>
2025-02-27 14:28:46 -0600

    updating buzzer kwargs

--- src/scout/buzzer.py
+++ src/scout/buzzer.py
@@ -3,4 +3,4 @@ import gpiozero
 
 class Buzzer(gpiozero.Buzzer):
     def __init__(self, pin):
-        super().__init__(pin=pin, active_high=False, initial_value=True)
+        super().__init__(pin=pin, active_high=False)
